eeb3a258987a9beca5109d5778cb34113e24634a
ClaraGnutzmann <claragnutzmann@yahoo.com.br>
2020-05-27 19:09:24 -0300

    limpar tela

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,28 +1,40 @@
 package aplication;
 
+import tabuleiro.TabuleiroExcecao;
 import xadres.PartidaXadres;
 import xadres.PecaXadres;
 import xadres.PosicaoXadres;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         PartidaXadres partidaXadres = new PartidaXadres();
-        UI.printTabuleiro(partidaXadres.getPecas());
 
         while (true){
-            UI.printTabuleiro(partidaXadres.getPecas());
-            System.out.println();
-            System.out.println("Origem: ");
-            PosicaoXadres origem = UI.lerPosicaoXadres(sc);
+            try {
+                UI.limparTela();
+                UI.printTabuleiro(partidaXadres.getPecas());
+                System.out.println();
+                System.out.println("Origem: ");
+                PosicaoXadres origem = UI.lerPosicaoXadres(sc);
 
-            System.out.println();
-            System.out.println("Destino: ");
-            PosicaoXadres destino = UI.lerPosicaoXadres(sc);
+                System.out.println();
+                System.out.println("Destino: ");
+                PosicaoXadres destino = UI.lerPosicaoXadres(sc);
 
-            PecaXadres pecaCapturada =  partidaXadres.performMovimentoXadres(origem, destino);
+                PecaXadres pecaCapturada = partidaXadres.performMovimentoXadres(origem, destino);
+            }
+            catch (TabuleiroExcecao e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+            catch (InputMismatchException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
 
 
